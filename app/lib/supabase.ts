1f5de6986335a6542c15b0a8120fccf797c61986
CruzBas <Warioregon123@gmail.com>
2026-03-12 10:43:51 -0600

    feat: Implement Supabase client, server, and middleware integration and update dev dependencies.

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Usamos las variables de entorno, con los valores reales como backup 
 // para evitar errores de DNS (ERR_NAME_NOT_RESOLVED) si el build no las detecta.
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://moxdocvtqtsapzasnwsx.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1veGRvY3Z0cXRzYXB6YXNud3N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTgwMzksImV4cCI6MjA4NzY5NDAzOX0.ragZkT_xQJSVGb90wM51UVmIZKz0jNudVcf8XUKCN9g';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://qwerquacvpipxgnynbro.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY || 'sb_publishable_i0q0ySPcKxCAsQaL1qJyNg_TX3pWsAX';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
